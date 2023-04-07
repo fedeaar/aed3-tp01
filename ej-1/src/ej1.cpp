9e685112869326533a80df00042c1a574f972432
fedeaar <fa.arienti@gmail.com>
2023-04-07 00:28:43 -0300

    mod ej1, corre en 35s!

--- ej-1/src/ej1.cpp
+++ ej-1/src/ej1.cpp
@@ -1,23 +1,32 @@
-//
-// Created by usuario1 on 4/4/2023.
-//
 #include "ej1.h"
 
-//Se declaran todas las variables globales para no tener que pasar como parámetro
 
-vector<bool> numerosUtilizados; //Este vector revisa si i ya está en el número mágico, para 0 <= i <= n*n
+//
+// GLOBAL
+//
+
+vector<bool>        numerosUtilizados; //Este vector revisa si i ya está en el número mágico, para 0 <= i <= n*n
 vector<vector<int>> cuadradoMagico;
-vector<int> sumasParciales; // Este vector guarda las sumas parciales de cada fila o columna, para revisar más
-                            // rápidamente la válidez del cuadrado. de tamaño 2n + 2, 0 <= i < n representa las
-                            // filas, n <= j < 2n representa las columnas y 2n, 2n+1 las diagonales
+vector<int>         sumasParciales;     // Este vector guarda las sumas parciales de cada fila o columna, para revisar más
+                                        // rápidamente la válidez del cuadrado. de tamaño 2n + 2, 0 <= i < n representa las
+                                        // filas, n <= j < 2n representa las columnas y 2n, 2n+1 las diagonales
+vector<int>         sumasMaximas;       // cada posicion i guarda cuánto más se podria sumar a la fila/columna/diagonal, 
+                                        // dado que ya tenemos i elementos determinados
+
 int limiteCuadrado;
 int numeroMagico;
 int cuadradosRestantes;
+
+
+//
+// UTILS
+//
 
 void generarCuadradoMagico(int n) {
     vector<vector<int>> cuadradoAHacer(n, vector<int>(n,0));
     cuadradoMagico = cuadradoAHacer;
 }
+
 
 void imprimirMatriz() {
     for (int i = 0; i < cuadradoMagico.size(); i++) {
@@ -28,94 +37,112 @@ void imprimirMatriz() {
     }
 }
 
-int sumarFilaActual(int x) {
-    int suma = 0;
-    for (int j = 0; j <= limiteCuadrado; j++) {
-        suma += cuadradoMagico[x][j];
-    }
-    return suma;
-}
 
-int sumarColActual(int y) {
-    int suma = 0;
-    for (int i = 0; i <= limiteCuadrado; i++) {
-        suma += cuadradoMagico[i][y];
-    }
-    return suma;
-}
+// 
+// PODAS
+//
 
-bool excedeAlNumeroMagico(int sumaFilaActual, int sumaColumnaActual, int x, int y) {
-    bool excede = (sumaFilaActual > numeroMagico) || (sumaColumnaActual > numeroMagico);
+bool excedeAlNumeroMagico(int x, int y, int i) {
+    int sumaFilaActual = sumasParciales[x] + i;
+    int sumaColActual  = sumasParciales[limiteCuadrado+1 + y] + i;
+    int sumDiagonal    = sumasParciales[(limiteCuadrado+1)*2] + i * (x == y);
+    int sumDiagonal2   = sumasParciales[(limiteCuadrado+1)*2 + 1] + i * (x + y == limiteCuadrado);
+    bool excede = (sumaFilaActual > numeroMagico) || (sumaColActual > numeroMagico) || (sumDiagonal > numeroMagico) || (sumDiagonal2 > numeroMagico);
     //Si todavia quedan numeros por llenar, es imposible que sea una solucion valida
     bool igualPeroQuedaEnFila = sumaFilaActual == numeroMagico && y < limiteCuadrado;
-    bool igualPeroQuedaEnColumna = sumaColumnaActual == numeroMagico && x < limiteCuadrado;
-    return excede || igualPeroQuedaEnFila || igualPeroQuedaEnColumna;
-}
-
-bool noSumaLoSuficiente(int sumaFilaActual, int sumaColumnaActual, int x, int y) {
-    return (y == limiteCuadrado && sumaFilaActual < numeroMagico) ||
-    (x == limiteCuadrado && sumaColumnaActual < numeroMagico);
+    bool igualPeroQuedaEnColumna = sumaColActual == numeroMagico && x < limiteCuadrado;
+    bool igualPeroQuedaEnDiagonal = sumDiagonal == numeroMagico && (x < limiteCuadrado || y < limiteCuadrado);
+    bool igualPeroQuedaEnDiagonal2 = sumDiagonal2 == numeroMagico && (x < limiteCuadrado);
+    return excede || igualPeroQuedaEnFila || igualPeroQuedaEnColumna || igualPeroQuedaEnDiagonal || igualPeroQuedaEnDiagonal2;
 }
 
 
-bool diagonalesValidas() {
-    return (sumasParciales[(limiteCuadrado+1)*2 + 1] == numeroMagico) &&
-    (sumasParciales[(limiteCuadrado+1)*2] == numeroMagico);
+bool noSumaLoSuficiente(int x, int y, int i) {
+    int sumaFilaActual = sumasParciales[x] + i;
+    int sumaColActual  = sumasParciales[limiteCuadrado+1 + y] + i;
+    int sumDiagonal    = sumasParciales[(limiteCuadrado+1)*2] + i * (x == y);
+    int sumDiagonal2   = sumasParciales[(limiteCuadrado+1)*2 + 1] + i * (x + y == limiteCuadrado);
+    return (sumaFilaActual + sumasMaximas[y] < numeroMagico) ||
+           (sumaColActual + sumasMaximas[x] < numeroMagico) ||
+           (x == y && sumDiagonal + sumasMaximas[x] < numeroMagico) ||
+           (x + y == limiteCuadrado && sumDiagonal2 + sumasMaximas[x] < numeroMagico);
 }
+
+
+//
+// SOLUCION
+//
 
 bool nEsimoCuadrado(int x, int y) {
-    if (x == limiteCuadrado +1) {
-        if(!diagonalesValidas()) return false;
-        if (cuadradosRestantes == 1) return true;
+    
+    // caso base, solo llegamos cuando es valido
+    if (x == limiteCuadrado + 1) {
+        return (--cuadradosRestantes) == 0;
+    }
 
-        cuadradosRestantes--;
-        return false;
-    }
-    for (int i = 1; i < numerosUtilizados.size(); i++) { //Se saltea el 0 porque no es parte del cuadrado
-        if (!numerosUtilizados[i]) { //Revisa si i no está todavía en el cuadrado mágico
-            int sumFilaActual = sumasParciales[x] + i;
-            int sumColActual = sumasParciales[limiteCuadrado+1 + y] + i;
-            //Si la fila/columna excede al número mágico, todos los valores posibles de i también lo harán
-            if(excedeAlNumeroMagico(sumFilaActual,sumColActual,x,y)) return false;
-            if (!noSumaLoSuficiente(sumFilaActual,sumColActual,x,y)) {
-                cuadradoMagico[x][y] = i;
-                numerosUtilizados[i] = true;
-                sumasParciales[x] += i;
-                sumasParciales[limiteCuadrado+1 + y] += i;
-                if (x == y) sumasParciales[(limiteCuadrado+1)*2] += i;
-                if (x + y == limiteCuadrado) sumasParciales[(limiteCuadrado+1)*2 + 1] += i;
-                if (y < limiteCuadrado) {
-                    if (nEsimoCuadrado(x, y + 1)) return true;
-                } else {
-                    if (nEsimoCuadrado(x + 1, 0)) return true;
-                }
-                sumasParciales[x] -= i;
-                sumasParciales[limiteCuadrado+1 + y] -= i;
-                if (x == y) sumasParciales[(limiteCuadrado+1)*2] -= i;
-                if (x + y == limiteCuadrado) sumasParciales[(limiteCuadrado+1)*2 + 1] -= i;
-                cuadradoMagico[x][y] = 0;
-                numerosUtilizados[i] = false;
-            }
+    for (int i = 1; i < numerosUtilizados.size(); i++) {
+        
+        if (numerosUtilizados[i]) continue;  // Revisa si i no está todavía en el cuadrado mágico
+            
+        // podas
+        // Si la fila/columna excede al número mágico, todos los valores posibles de i también lo harán
+        if (excedeAlNumeroMagico(x, y, i)) return false;
+        // Si ya no podemos llegar al numero magico, descartamos
+        if (noSumaLoSuficiente(x, y, i)) continue;
+
+        // resultado parcial
+        numerosUtilizados[i] = true; // Esta rama ya no va a poder utilizar este valor
+        cuadradoMagico[x][y] = i;
+
+        // actualizamos sumas parciales
+        sumasParciales[x] += i;
+        sumasParciales[limiteCuadrado+1 + y] += i;
+        if (x == y) sumasParciales[(limiteCuadrado+1)*2] += i;
+        if (x + y == limiteCuadrado) sumasParciales[(limiteCuadrado+1)*2 + 1] += i;
+
+        // recurrimos
+        if (y < limiteCuadrado) {
+            if (nEsimoCuadrado(x, y + 1)) return true; // short circuit
+        } else {
+            if (nEsimoCuadrado(x + 1, 0)) return true;
         }
+
+        // deshacemos cambios
+        sumasParciales[x] -= i;
+        sumasParciales[limiteCuadrado+1 + y] -= i;
+        if (x == y) sumasParciales[(limiteCuadrado+1)*2] -= i;
+        if (x + y == limiteCuadrado) sumasParciales[(limiteCuadrado+1)*2 + 1] -= i;
+
+        // liberamos el valor
+        numerosUtilizados[i] = false;
     }
+
     return false;
 }
 
+
 void resolver(int n,int k) {
-    //Se inicializan todas las variables globales
+
+    // Se inicializan todas las variables globales
     generarCuadradoMagico(n);
     limiteCuadrado = n-1;
-    for (int i = 0; i < (n*n)+1; i++) {
-        numerosUtilizados.push_back(false);
+    numerosUtilizados.resize(n*n+1, false);
+    sumasParciales.resize(2*n + 2, 0);
+    sumasMaximas.reserve(n);
+    sumasMaximas[n-1] = 0;
+    for (int i = 1; i < n; ++i) {
+        sumasMaximas[n - i - 1] = sumasMaximas[n - i] + n*n - i + 1;
     }
-    for (int i = 0; i < 2*n + 2; i++) {
-        sumasParciales.push_back(0);
-    }
-    numeroMagico = (n*n*n + n)/2;
+    numeroMagico = (n*n*n + n) / 2;
     cuadradosRestantes = k;
+
+    // calculamos
+    auto start = chrono::system_clock::now();
     bool existeSolucion = nEsimoCuadrado(0,0);
     if (existeSolucion) imprimirMatriz();
     else {
-        cout << -1;
+        cout << -1 << endl;
     }
+    auto end = chrono::system_clock::now();
+    cout << chrono::duration_cast<chrono::milliseconds>(end - start).count() << "ms" << endl;   
 }
--- ej-1/src/ej1.h
+++ ej-1/src/ej1.h
@@ -1,29 +1,25 @@
-//
-// Created by usuario1 on 4/4/2023.
-//
-
 #ifndef EJ_1_EJ1_H
 #define EJ_1_EJ1_H
 
+
 #include <iostream>
 #include <vector>
+#include <chrono>
+
 using namespace std;
 
-bool excedeAlNumeroMagico(int sumaFilaActual, int sumaColumnaActual, int x, int y);
-
-bool noSumaLoSuficiente(int sumaFilaActual, int sumaColumnaActual, int x, int y);
-
-int sumarColActual(int y);
-
-int sumarFilaActual(int x);
-
-bool diagonalesValidas();
 
 void generarCuadradoMagico(int n);
 
-void resolver(int n, int k);
+void imprimirMatriz();
+
+bool excedeAlNumeroMagico(int x, int y, int i);
+
+bool noSumaLoSuficiente(int x, int y, int i);
 
 bool nEsimoCuadrado(int x, int y);
 
-void imprimirMatriz();
+void resolver(int n, int k);
+
+
 #endif //EJ_1_EJ1_H
