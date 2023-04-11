9863ce476711c1b41106e3a7efb2763fa26dbbc3
fedeaar <fa.arienti@gmail.com>
2023-04-11 14:21:49 -0300

    add tests

--- ej-2/main.cpp
+++ ej-2/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <random>
+
 
 typedef long long longl;
 
@@ -10,12 +12,13 @@ typedef long long longl;
 
 longl c = 0, n = 0, r, m;
 std::vector<longl> V;
-std::vector<std::vector<short>> M;
+std::vector<std::vector<char>> M;
 
 
 //
 // SOLUCION
 //
+
 
 longl modexp(longl b, longl e, longl m) {
     // idea: metodo binario l-r
@@ -23,6 +26,7 @@ longl modexp(longl b, longl e, longl m) {
         return 0;
     }
     longl r = 1;
+    b = b % m;
     while (e > 0) {
         if (e % 2 == 1) {
             r = (b * r) % m;
@@ -52,9 +56,81 @@ bool solucion(longl k, longl s) {
 bool resolver() {
     M.resize(n);
     for (longl i = 0; i < n; ++i) {
-        M[i] = std::vector<short>(m, -1);
+        M[i] = std::vector<char>(m, -1);
     }
     return solucion(0, V[0] % m);
+}
+
+
+// TESTS
+
+longl powxy(longl x, longl y, longl m) { // otra imple de modexp para comparar
+    if (y == 0) return 1;
+    if (y%2 == 1) return (x*powxy(x, y-1, m)) % m;
+    longl t = powxy(x, y/2, m);
+    return (t*t) % m;
+}
+
+void test(int cantidad) {
+    // cosas para RNG
+    std::random_device dev;
+    std::mt19937 rng(dev());
+    std::uniform_int_distribution<std::mt19937::result_type> distN(1, 10000);
+    std::uniform_int_distribution<std::mt19937::result_type> distM(1, 100000);
+    std::uniform_int_distribution<std::mt19937::result_type> dist4(1, 4);
+    // tests
+    for (int i = 0; i < cantidad; ++i) {
+        n = distN(rng);
+        m = distM(rng);
+        V.resize(n);
+        std::vector<int> ops(n - 1);
+        
+        V[0] = distN(rng);
+        longl res = V[0];
+        for (int j = 1; j < n; ++j) {
+            V[j] = distN(rng);
+            ops[j-1] = dist4(rng);
+            switch (ops[j-1]) {
+                case(1):
+                    res = res + V[j];
+                    break;
+                case(2):
+                    res = res - V[j];
+                    break;
+                case(3):
+                    res = res * V[j];
+                    break;
+                case(4):
+                    res = powxy(res, V[j], m);
+                    break;
+            }
+        }
+        r = (m + res % m) % m;
+
+        std::cout << "test: " << i << ", n: " << n << ", r: " << r << ", m: " << m << ", mem M: " << n * m * sizeof(char) / 1e6 << " mb." << std::endl;
+        
+        if (!resolver()) {
+            std::cout << "\t error, posible solucion: ";
+            for (int j = 0; j < n-1; ++j) {
+                std::cout << V[j];
+                switch(ops[j]) {
+                    case(1):
+                        std::cout << '+';
+                        break;
+                    case(2):
+                        std::cout << '-';
+                        break;
+                    case(3):
+                        std::cout << '*';
+                        break;
+                    case(4):
+                        std::cout << '^';
+                        break;
+                }
+            }
+            std::cout << V[n-1] << std::endl;
+        }
+    }
 }
 
 
@@ -63,20 +139,21 @@ bool resolver() {
 //
 
 int main(int argc, char** argv) {
-    std::cin >> c;
-    for (longl i = 0; i < c; ++i) {
-        std::cin >> n >> r >> m;
-        V.resize(n);
-        for (longl j = 0; j < n; ++j) {
-            std::cin >> V[j]; 
-        }
-        std::string rta;
-        if (resolver()) {
-            rta = "Si";
-        } else {
-            rta = "No";
-        }
-        std::cout << rta << std::endl;
-    }
+    // std::cin >> c;
+    // for (longl i = 0; i < c; ++i) {
+    //     std::cin >> n >> r >> m;
+    //     V.resize(n);
+    //     for (longl j = 0; j < n; ++j) {
+    //         std::cin >> V[j]; 
+    //     }
+    //     std::string rta;
+    //     if (resolver()) {
+    //         rta = "Si";
+    //     } else {
+    //         rta = "No";
+    //     }
+    //     std::cout << rta << std::endl;
+    // }
+    test(1000);
     return 0;
 }
